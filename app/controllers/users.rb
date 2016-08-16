cdff1d887c61d22bbd71bd89186c366d0820a68d
Craig Waterman <email": null>
2016-08-15 19:35:34 -0500

    user controller and view

---
+++ app/controllers/users.rb
@@ -0,0 +1,56 @@
+get '/users/new' do
+  erb :'users/new'
+end
+
+post '/users' do
+  user = User.new(params[:user])
+  if user.save
+    session[:user_id] = @user.id
+    redirect '/'
+  else
+    @errors = user.errors.full_messages
+    erb :'/users/new'
+  end
+end
+
+get '/users/login' do
+  erb :'users/login'
+end
+
+post '/users/login' do
+  @user = User.authenticate(params[:user][:email], params[:user][:password])
+  if @user
+    session[:user_id] = @user.id
+    redirect '/'
+  else
+    @error = "Invalid entries"
+    erb :'users/login'
+  end
+end
+
+get '/users/logout' do
+  session[:user_id] = nil
+  redirect '/'
+end
+
+get '/not_authorized' do
+  erb :not_authorized
+end
+
+get '/users/:user_id' do
+  @user = User.find(params[:user_id])
+  erb :'users/index'
+end
+
+
+get "/users/:id/edit" do
+  authorize!
+  erb :"/users/edit"
+end
+
+put "/users/:id" do
+  authorize!
+  @user = User.find(params[:id])
+  @user.update(params[:user])
+  redirect "/users/params[:id]"
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,34 @@ include BCrypt
   has_many  :bids, class_name: :Bid, foreign_key: :bidder_id
   has_many  :auctions, foreign_key: :seller_id
 
+  validates :email, presence:true, uniqueness:true, format: { with: /\S+@\S+/, message: "Not valid email address." }
+  validates_presence_of :username, uniqueness:true
+  validates_presence_of :hashed_password
+  validate :password_longer_than_six
+
+  def password
+    @password ||= Password.new(hashed_password)
+  end
+
+  def password=(new_password)
+    @plaintext = new_password
+    @password = Password.create(new_password)
+    self.hashed_password = @password
+  end
+
+  def self.authenticate(email,password)
+    @user = User.find_by(email:email)
+    return nil unless @user
+    if @user.password == password
+      return @user
+    end
+  end
+
+  def password_longer_than_six
+    if @plaintext.length < 6
+      self.errors.add(:password, "Password must be at least six characters")
+    end
+  end
+
 end
 
